4f1ae12ce61ac4a2060420540cc0f3ac45d7d0e9
gmaxstores <maxwellnvani@gmail.com>
2025-01-14 09:44:39 +0100

    fixed the broken cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-const cartList = [];
 
 function addProductToCart(product) {
-  
-  setLocalStorage("so-cart", cartList);
+  const currentCart = getLocalStorage("so-cart") || [];
+  currentCart.push(product);
+  setLocalStorage("so-cart", currentCart);
 }
 
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  cartList.push(product);
   addProductToCart(product);
 }
 
