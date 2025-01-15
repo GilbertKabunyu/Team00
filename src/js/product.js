2b1d8b84a4e781462fd8c60ef02af24edd6fbc36
gmaxstores <maxwellnvani@gmail.com>
2025-01-15 15:37:10 +0100

    aligned my code with the team

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,24 @@
-import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getParams } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParams("product");
 
-// add to cart button event handler
-
-// add listener to Add to Cart button
-//document
-//  .getElementById("addToCart")
-//  .addEventListener("click", addToCartHandler);
-
-const productId = getParam("product");
-//console.log(dataSource.findProductById(productId));
 const product = new ProductDetails(productId, dataSource);
 product.init();
+
+function addProductToCart(product) {
+  console.log(product);
+  setLocalStorage("so-cart", product);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
