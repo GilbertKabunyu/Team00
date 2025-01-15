41f21bca038cb2e709a6d0dbcb33679d0c12e6db
gmaxstores <maxwellnvani@gmail.com>
2025-01-15 09:14:25 +0100

    fixed the add to cart button by adding the addtocart handler function as a method in ProductDetails (Week 2 team activity)

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,17 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  const currentCart = getLocalStorage("so-cart") || [];
-  currentCart.push(product);
-  setLocalStorage("so-cart", currentCart);
-}
-
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+//document
+//  .getElementById("addToCart")
+//  .addEventListener("click", addToCartHandler);
 
+const productId = getParam("product");
+//console.log(dataSource.findProductById(productId));
+const product = new ProductDetails(productId, dataSource);
+product.init();
