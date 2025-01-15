6ab03a5187f0c18f86bcf072a3748e55a6bfe34b
ManGuevara <mguevaradelacruz@gmail.com>
2025-01-14 23:52:29 -0500

    adding procuts on products.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-
+const cartData = getLocalStorage("so-cart");
+console.log(cartData);
+if (cartData == null) {
+  setLocalStorage("so-cart", []);
+}
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart= getLocalStorage("so-cart");
+  if (cart.length == 0){
+    setLocalStorage("so-cart", [product]);
+  }
+  else {
+    setLocalStorage("so-cart", [...cart, product]);
+  }
+  //setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
