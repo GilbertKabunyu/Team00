42d10114a38e5c67f3092635e0c29aa20edba594
Helamã Y. Barbour <helamashi@gmail.com>
2025-01-17 11:40:18 -0300

    Made the buttons to remove items from the cart.
    The function removeCartItem is working, the only problem is the addeventListener. The eventListener isn't getting assigned a "key" value, making it impossible to remove an item from the cart.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,11 @@
 import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.getElementById("880RR").addEventListener("click", removeCartItem());
 }
 
 function cartItemTemplate(item) {
@@ -17,12 +19,19 @@ function cartItemTemplate(item) {
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+    <button class="remove-button" type="button"><span id="${item.Id}">X</span></button>
 </li>`;
 
   return newItem;
+}
+
+function removeCartItem() {
+  const storedItem = getLocalStorage("so-cart" || []);
+  const updatedItem = storedItem.filter((item) => item.Id !== item.Id);
+  setLocalStorage("so-cart", updatedItem);
 }
 
 renderCartContents();
