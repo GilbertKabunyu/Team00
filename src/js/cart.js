d5c7976b20ca0ddd5fb7255841b74d150d47d980
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-01-18 01:52:14 +0000

    Added the for loop so that each button have it's own event handler that will pull it's specified ID, making the function "removeCartItem" to only remove the item from the localStarage that is related to the Id of the product/button assigned.
    Still, it looks like that the buttons is not "remembering" the assigned event and because of that, nothing is happening when clicking the remove button.

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,7 +6,15 @@ function renderCartContents() {
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   //Event listener to remove item from Local Storage
-  document.getElementById("880RR").addEventListener("click", removeCartItem());
+  const removeButtons = document.querySelectorAll(".remove-button");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", removeCartItem);
+  })
+  //if (document.getElementById("880RR") === null) {
+  //  console.log("There is no items in the cart list");
+  //} else {
+//document.getElementById("880RR").addEventListener("click", removeCartItem);
+  //}
 }
 
 function cartItemTemplate(item) {
@@ -23,17 +31,18 @@ function cartItemTemplate(item) {
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     <p class="cart-card__quantity">qty: 1</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
-    <button class="remove-button" type="button"><span id="${item.Id}">X</span></button>
+    <button class="remove-button" id="${item.Id}" type="button"><span>X</span></button>
 </li>`;
 
   return newItem;
 }
 
-function removeCartItem() {
+function removeCartItem(event) {
+  const itemId = event.target.id
   //Gets the items from the local storage
   const storedItem = getLocalStorage("so-cart" || []);
   //Updates the localStorage by removing the object with the Item.Id searched
-  const updatedItem = storedItem.filter((item) => item.Id !== item.Id);
+  const updatedItem = storedItem.filter((item) => item.Id !== itemId);
   //Sends the updated version to the localStorage.
   setLocalStorage("so-cart", updatedItem);
 }
