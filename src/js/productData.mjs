5f411eaa9a825df1be4db7c9ceb3dbc54cd7bc38
Helamã Y. Barbour <helamashi@gmail.com>
2025-01-24 11:29:06 -0300

    changed the class ProductData so that it has something to look for. It had nothing before.

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -9,8 +9,9 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor() {
-
+  constructor(category) {
+    this.category = category;
+    this.path = `../json/${this.category}.json`;
   }
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
