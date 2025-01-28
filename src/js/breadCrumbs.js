61f26c4ec607ab36a3eb3081d2a7ba8a1bbbff46
Helamã Y. Barbour <helamashi@gmail.com>
2025-01-28 10:02:10 -0300

    Finished the BreadCrumb

--- src/js/breadCrumbs.js
+++ src/js/breadCrumbs.js
@@ -11,35 +11,42 @@ export function breadCrumb() {
         //Create in the localStorage a current-page key with the link of the current webpage
         setLocalStorage("current-page", window.location.href);
 
-        //If in the local storage the link in the current-page key is not equal to /index.html or /
-    } else if (getLocalStorage("current-page") !== "/index.html" || getLocalStorage("current-page") !== "/") {
-
-        //Create and store in local storage an key called last-page and stores the link inside the current-page key from local storage
-        setLocalStorage("last-page", getLocalStorage("current-page"));
-
-        //Set the current-page localStorage key with the current link in the webpage.
-        setLocalStorage("current-page", window.location.href);
-
         // This should be called whne selecting a product, but it isn't being called. You finish this and you're done.
-    } else if (getLocalStorage("list-items" !== window.location.search)) {
+    } else if (getLocalStorage("list-items")) {
+
+        //Change the inner HTML from the breadCrumb so that it shows the category from the old page that the user entered
+        link.innerHTML = getLocalStorage("list-items");
 
         //Gets the category name from the page and stores it into param
         const param = getParams("category");
 
+        //Creates a list-items key for localStorage and stores the category from the old page.
+        setLocalStorage("list-items", param);
+
         //Create and store in local storage an key called last-page and stores the link inside the current-page key from local storage
         setLocalStorage("last-page", getLocalStorage("current-page"));
 
         //Set the current-page localStorage key with the current link in the webpage.
         setLocalStorage("current-page", window.location.href);
 
+        //Add an event for when the user clicks that category to go back to that page.
+        link.addEventListener("click", changeURL(link));
+
+        //If in the local storage the link in the current-page key is not equal to /index.html or /
+    } else if (getLocalStorage("current-page") === "/index.html" || getLocalStorage("current-page") !== "/") {
+
+
+        //Gets the category name from the page and stores it into param
+        const param = getParams("category");
+
         //Creates a list-items key for localStorage and stores the category from the old page.
         setLocalStorage("list-items", param);
 
-        //Change the inner HTML from the breadCrumb so that it shows the category from the old page that the user entered
-        link.innerHTML = getLocalStorage("list-items");
-        
-        //Add an event for when the user clicks that category to go back to that page.
-        link.addEventListener("click", changeURL(link));
+        //Create and store in local storage an key called last-page and stores the link inside the current-page key from local storage
+        setLocalStorage("last-page", getLocalStorage("current-page"));
+
+        //Set the current-page localStorage key with the current link in the webpage.
+        setLocalStorage("current-page", window.location.href);
 
     } else {
         console.log("There is no link")
